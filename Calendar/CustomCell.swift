84d1ecb8452719380ff234ee15f985c331fbbaae
Andy Wu <andywu.05@gmail.com>
2018-04-25 23:15:25 -0400

    Initial commit

---
+++ Calendar/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  Calendar
+//
+//  Created by Andy on 4/22/18.
+//  Copyright © 2018 Andy. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+}
--- Calendar/ViewController.swift
+++ Calendar/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthLbl: UILabel!
+    @IBOutlet weak var yearLbl: UILabel!
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCalendarView()
     }
 
+    func setupCalendarView() {
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+    }
+    
+    func handleCellSelected(view: JTAppleCell, cellState: CellState) {
+        guard let validCell = view as? CustomCell else { return }
+        
+        if validCell.isSelected {
+            validCell.selectedView.isHidden = false
+        } else {
+            validCell.selectedView.isHidden = true
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+}
+
+// MARK : JTAppleCalendarDelegate
+extension ViewController: JTAppleCalendarViewDataSource {
+    public func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text = cellState.text
+    }
+
+    public func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2018 01 01")!
+        let endDate = formatter.date(from: "2018 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+    
+}
+
+extension ViewController: JTAppleCalendarViewDelegate {
+    // Display the cell
+    public func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text = cellState.text
+        
+        handleCellSelected(view: cell, cellState: cellState)
+        
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellSelected(view: cell!, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellSelected(view: cell!, cellState: cellState)
+    }
 }
 
